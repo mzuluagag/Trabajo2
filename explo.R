f5ab9fd2f33bc5e9278b507e656ef0ebd34a22d8
Santiago <sanrestrepo98@gmail.com>
2018-10-07 22:45:42 -0500

    1

--- explo.R
+++ explo.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(dplyr)
+library(Hmisc)
 library(lubridate)
 setwd("D:/Documents/GitHub/Trabajo2")
 db <- read.csv2("Autos TRM.csv",header = TRUE,stringsAsFactors = FALSE)
@@ -7,13 +8,34 @@ db$Fecha <- as.Date(db$Fecha, format = "%d/%m/%Y") #Formating the date
 db$TRM=gsub("\\$", "", db$TRM)
 db$TRM=gsub("\\.", "", db$TRM)
 db$TRM=as.numeric(gsub("\\,", ".", db$TRM))
-dbmeses<-db%>% group_by(Mes=floor_date(Fecha, "month")) %>%summarize(Unidades=sum(Unidades),TRM=mean(TRM))
+db<-cbind2(db,month(db$Fecha))
+db<-cbind2(db,weekdays(db$Fecha))
+names(db)[4]<-"mes"
+#Creación de dataframe por meses
+attach(db)
+dbmeses<-db%>%group_by(Mes=floor_date(Fecha,"month"))%>%summarise(Unidades=sum(Unidades),TRM=mean(TRM))
 dbmeses$Mes<-as.Date(dbmeses$Mes)
 googleaux<-read.csv2("multiTimeline (1).csv",header=T,sep=",")
 dbmeses<-cbind2(dbmeses,googleaux$carro...Colombia.)
 names(dbmeses)[2]<-"uni"
 names(dbmeses)[4]<-"pop"
 names(dbmeses)[1]<-"mes"
+#Incluir popularidad en el dataframe por día
+vpop<-rep(dbmeses$pop[1],monthDays(dbmeses$mes[1]))
+for (i in 2:nrow(dbmeses)){
+  auxpop<-rep(dbmeses$pop[i],monthDays(dbmeses$mes[i]))
+  vpop<-append(vpop,auxpop)
+}
+db<-cbind2(db,vpop)
+names(db)[6]<-"pop"
+empleoaux<-read.csv2("Empleo_desempleo.csv",header=FALSE,stringsAsFactors = FALSE)
+names(empleoaux)[1]<-"fecha"
+names(empleoaux)[2]<-"templeo"
+names(empleoaux)[3]<-"tdesem"
+empleoaux$fecha<-as.Date(paste(empleoaux$fecha,1,sep="-"),"%Y-%m-%d")
+empleoaux<-empleoaux[order(empleoaux$fecha),]
+
+
 attach(dbmeses)
 plot(mes,TRM,type="l",lwd=2)
 plot(mes,uni,type="l",col="red",lwd=2)
